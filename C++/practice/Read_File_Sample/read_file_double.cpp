f8a05605e0a1e017b6154791f891b9f334878514
Zeng <1103105339@nkust.edu.tw>
2019-03-22 23:38:28 +0800

    read file(1.2,5.7,9.3) sample

---
+++ C++/practice/Read_File_Sample/read_file_double.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string> 
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+int main()
+{
+  vector<double> matrix;
+  //readfile
+  fstream file;
+  file.open("data.txt");
+  string line;
+  while (getline( file, line))  //讀一整行放進line
+	{
+	  istringstream templine(line); // string(line) 轉換成 stringstream
+	  string data;
+	  while (getline( templine, data,',')) //切逗號放進data
+	  {
+	    matrix.push_back(atof(data.c_str()));  //string 轉換成數字 存進vector
+	  }
+	}
+  file.close();
+  
+  for (size_t i=0;i<matrix.size();i++)
+  {
+    cout << matrix[i]<<" ";
+  }	
+  return 0;
+}
