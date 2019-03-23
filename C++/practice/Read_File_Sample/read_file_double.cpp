381796402d54c63f0dc5516290802e50f9d4836d
Zeng <1103105339@nkust.edu.tw>
2019-03-23 12:03:03 +0800

    read_file makefile update

--- C++/practice/Read_File_Sample/read_file_double.cpp
+++ C++/practice/Read_File_Sample/read_file_double.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <math.h>
+#include <stdlib.h>
 using namespace std;
 int main()
 {
@@ -22,9 +24,10 @@ int main()
 	}
   file.close();
   
-  for (size_t i=0;i<matrix.size();i++)
+  for (size_t i=0;i<matrix. size();i++)
   {
     cout << matrix[i]<<" ";
   }	
+  cout<<endl;
   return 0;
 }
