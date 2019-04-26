76148b805ef053b2194953d737985d30757e5efb
Hui <1103105339@nkust.edu.tw>
2019-04-26 16:55:58 +0800

    multi and ordinary compution of 2D array

--- C++/C++ test/test.cpp
+++ C++/C++ test/test.cpp
@@ -8,23 +8,68 @@
 #include <string.h>
 #include <stdio.h>
 #include <thread>
+#include <time.h>
 using namespace std;
-size_t range = 10;
+size_t range = 100;
 
-void func(int index, const vector< vector<double> >&data , vector<double>&result)
+void func(int index, const vector<vector<double>> &data, vector<double> &result)
 {
-	cout << "new thread :" << index << endl;
-	
+	//cout << "new thread :" << index << endl;
+
 	for (size_t i = 0; i < range; i++)
 	{
-		result[index] += data[index][i];
+		result[index] += (data[index][i] / range)*2;
 	}
-	
-	
+	string name = to_string( index ) + "thread_out.txt";
+	fstream file;
+	file.open(name.c_str(), ios::out);
+	if (!file)
+	{
+		cout << "error open" << endl;
+	}
+	else
+	{
+		for (size_t i = 0; i < range; i++)
+		{
+			file << data[index][i] << endl;
+		}
+		 file.close();
+	}
 }
+
+void normal(const vector<vector<double>> &data, vector<double> &result)
+{
+	for (size_t i = 0; i < range; i++)
+	{
+		for (size_t j = 0; j < range; j++)
+		{
+			result[i] += (data[i][j] / range)*2;
+		}
+		string name = to_string(i) + "ordinary_out.txt";
+		fstream file;
+		file.open(name.c_str(), ios::out);
+
+		if (!file)
+		{
+			cout << "error open" << endl;
+		}
+		else
+		{
+			for (size_t k = 0; k < range; k++)
+			{
+				file << data[i][k]<< endl;
+			}
+			file.close();
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	vector< vector<double> > data;
+	double START, END;
+	
+
+	vector<vector<double>> data;
 	data.resize(range);
 	for (size_t i = 0; i < range; i++)
 	{
@@ -32,30 +77,39 @@ int main(int argc, char *argv[])
 	}
 	for (size_t i = 0; i < range; i++)
 	{
-		for (size_t j = 0; j < range; i++)
+		for (size_t j = 0; j < range; j++)
 		{
-			data[i][j] = i + j;
+			data[i][j] = i + (j + 1);
 		}
 	}
 
-	vector<double> average ;
+	vector<double> average;
 	average.resize(range);
-
+	
 	vector<thread> threads;
+	threads.reserve(range);
 	for (size_t i = 0; i < range; i++)
 	{
-		threads.push_back(thread(func, i, ref(data) , ref(average) ));
+		threads.push_back(thread(func, i, ref(data), ref(average)));
 	}
+	START = clock();
+	
 	for (size_t i = 0; i < threads.size(); i++)
 	{
 		threads[i].join();
 	}
+	
+	//normal(data, average);
+
+	END = clock();
+	
 
 	for (size_t i = 0; i < range; i++)
 	{
-		cout<<average[i]<<endl;
+		//cout << average[i] << endl;
 	}
-	
+	cout << endl
+		 << "time comsuming ： " << (END - START) / CLOCKS_PER_SEC << " S" << endl;
 	//system("vim data.txt");
 	return 0;
 }
